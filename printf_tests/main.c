322e810d745366003ca359911cf0226459538ffd
Ilyas Founas <ifounas@z1r11p3.42lyon.fr>
2024-12-18 10:35:09 +0100

    GnlTester and PrintfTester

---
+++ printf_tests/main.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ifounas <ifounas@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/26 15:38:00 by ifounas           #+#    #+#             */
+/*   Updated: 2024/12/16 15:53:48 by ifounas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <fcntl.h>
+# include <stdio.h>
+// cc -Wall -Wextra -Werror main.c ft_printf.c ft_printf_utils.c  ; ./a.out
+int	main(void)
+{
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        CHAR                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+	char c = '0';
+	printf("\nnb_words : %d \n\n", ft_printf("%c", c));
+	printf("\nnb_words : %d \n", printf("%c", c));
+
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        STRING                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+
+	printf("\nnb_words : %d \n", ft_printf("%s", "a"));
+	printf("\nnb_words : %d \n", printf("%s", "a"));
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        U INTEGER                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+
+	printf("\nnb_words : %d \n\n", ft_printf("%u ", -1));
+	printf("\nnb_words : %d \n\n", printf("%u ", -1));
+
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        LOW HEXA                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+	printf("\nnb_words : %d \n\n", ft_printf("%x", -42));
+	printf("\nnb_words : %d \n", printf("%x", -42));
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        UPP HEXA                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+	printf("\nnb_words : %d \n\n", ft_printf("%X", -42));
+	printf("\nnb_words : %d \n", printf("%X", -42));
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        INTEGER                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+
+	printf("\nnb_words : %d \n\n", ft_printf("%d", -42));
+	printf("\nnb_words : %d \n", printf("%d", -42));
+
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        PTR                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+
+	//int x = 12416;
+	printf("\nnb_words : %d \n\n", ft_printf("%p", NULL));
+	printf("\nnb_words : %d \n", printf("%p", NULL));
+
+	printf("\n\n |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n");
+	printf("                        PRINT 0                        \n");
+	printf(" |||||||||||||||||||||||||||||||||||||||||||||||||||||||| \n\n");
+
+	printf("\nnb_words : %d \n\n", ft_printf(0));
+	printf("\nnb_words : %d \n", printf(0));
+}
